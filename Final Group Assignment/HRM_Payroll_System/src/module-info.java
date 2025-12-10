37d422f352d84587f2d4dc3ff56147140263658c
Garvdudy <garvdudy47@gmail.com>
2025-12-10 18:57:32 -0500

    Updating the project

--- Final Group Assignment/HRM_Payroll_System/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module HRM_Payroll_System {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-
-    opens org.example.hrm_payroll_system to javafx.fxml;
-    exports org.example.hrm_payroll_system;
-}
